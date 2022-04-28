3951e8d475bebba6bf1e566ddb6522c6b6b91378
Tony Grimes <acidtone@tonygrimes.com>
2022-04-28 02:15:09 -0600

    added count of push events

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,14 @@
 <script>
   export let events;
+  export let count = events.reduce((previous, item) => {
+    if (item.type === 'PushEvent') return previous + 1;
+    return previous;
+  }, 0);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<strong>Total Push Events: {count}</strong>
 <ul>
   {#each events as event}
   <li>{event.created_at}: {event.type}</li>
